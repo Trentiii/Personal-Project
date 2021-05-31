fdfb0b60ee398770d0243757c23b3bcfa3022b32
Trentiii <1707241@mukilteo.wednet.edu>
2021-05-30 20:14:19 -0700

    Update BetterMovement.cs, BetterMovement.cs.meta, and 16 more files...

---
+++ Assets/PlayerFlip.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFlip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {  //This short code makes player flip when the A and D or arrow keys are pressed.
+
+        Vector3 charecterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            charecterScale.x = -1;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            charecterScale.x = 1;
+        }
+        transform.localScale = charecterScale;
+    }
+}
